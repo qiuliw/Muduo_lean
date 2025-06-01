000aa0b78d71f08dbd7d7e029012ca31014b8b63
qiuliw <1807191473@qq.com>
2025-06-01 19:15:01 +0800

    EventLoopThreadPool

---
+++ myMuduo/EventLoopThreadPool.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "EventLoop.h"
+#include "noncopyable.h"
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+class EventLoop;
+class EventLoopThread;
+
+class EventLoopThreadPool : noncopyable{
+
+public:
+    using ThreadInitCallback = std::function<void(EventLoop*)>;
+
+    // baseLoop_ 用户提供的Loop
+    EventLoopThreadPool(EventLoop *baseLoop_,const std::string &nameArg);
+    ~EventLoopThreadPool();
+
+    void setThreadNum(int numThreads){
+        numThreads_ = numThreads;
+    }
+
+    void start(const ThreadInitCallback &cb = ThreadInitCallback());
+
+    // 如果是多线程中，baseLoop会默认以轮询方式分配chnnel给subloop
+    EventLoop* getNextLoop();
+
+    std::vector<EventLoop*> getAllLoops();
+
+    bool started() const { return started_;}
+    const std::string name() const { return name_;}
+
+private:
+
+    // 默认单线程模型
+    EventLoop *baseLoop_; // EventLoop loop
+    std::string name_;
+    bool started_; 
+    int numThreads_;
+    int next_;
+    std::vector<std::unique_ptr<EventLoopThread>> threads_;
+    std::vector<EventLoop*> loops_;
+
+};
+
